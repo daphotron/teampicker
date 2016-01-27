1f8daca13008fe00376fc644db0d2d8e2a5b76b7
Daphne and Brian <daphnehello@gmail.com + polymonic@gmail.com>
2016-01-26 20:02:16 -0500

    Add people and team counter

---
+++ www/js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('starter.controllers', [])
+.controller('TeamsController', function($scope) {
+  console.log('hi');
+  $scope.peopleCount = 4;
+  $scope.teamCount = 2;
+
+  $scope.subtractPeople = function() {
+    if ($scope.peopleCount > 2) {
+      $scope.peopleCount--;
+    }
+  };
+
+  $scope.addPeople = function() {
+    $scope.peopleCount++;
+  };
+
+  $scope.subtractTeams = function() {
+    if ($scope.teamCount > 2) {
+      $scope.teamCount--;
+    }
+  };
+
+  $scope.addTeams = function() {
+    $scope.teamCount++;
+  };
+});
